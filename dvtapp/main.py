c1cc15eaf757284e6b370dd8176e36a01c0bfbc0
julien decot <jdecotpro@gmail.com>
2024-12-25 23:33:35 +0100

    update documentation

--- dvtapp/main.py
+++ dvtapp/main.py
@@ -9,34 +9,45 @@ import os
 import signal
 
 def process_json_line(element):
+    """
+    This function processes a single JSON data line, compares its schema with the BigQuery table schema,
+    adds missing columns if necessary, and inserts the data into BigQuery.
+    """
     data = json.loads(element)
     print("------------------- New Row ----------------------")
     print("data : ", data)
+
+    # Compare the data's keys with the schema of the BigQuery table
     missing_columns = manage_table.compare_dict_to_schema(bq_client, dataset_id, table_id, data)
     print("missing_columns : ", missing_columns)
 
+    # For each missing column, add it to the BigQuery table
     for column in missing_columns:
         SchemaField = schemafield_from_value.schemafield_from_value(column, data[column])
         manage_table.add_column_to_table(bq_client, dataset_id, table_id, column, SchemaField)
 
+    # Insert the data into BigQuery with retry logic in case of failure
     manage_table.insert_data_with_retry(bq_client, dataset_id, table_id, data)
 
 
 def trigger_pipeline(filename):
-    print("fct trigger pipeline, filename : ", filename)
+    """
+    This function initializes the BigQuery client, creates or deletes tables as needed,
+    and starts the Apache Beam pipeline to process the JSON file.
+    """
 
-    # Define Parameters
-    global project_id, region, dataset_id, bq_client, table_id
-    project_id = "dvtapp"
-    region = "us-east1"
-    dataset_id = "ds_dvtapp"
+    # Define BigQuery client and the table to create his name 
+    global bq_client, table_id
     bq_client = bigquery.Client(project=project_id)
     table_id = manage_table.create_table_name() 
 
-    # Delete tables in dataset and create a new one
+    # Delete all existing tables in the dataset and create a new one
     manage_table.delete_all_tables(bq_client, project_id, dataset_id)  
     manage_table.create_table_if_not_exists(bq_client, dataset_id, table_id, initial_schema=[])
     
+    # Start the Apache Beam pipeline to process the JSON file
+    # Read the JSON file from Cloud Storage
+    # Process each JSON line
     with beam.Pipeline(options=PipelineOptions()) as pipeline:
         (
             pipeline
@@ -46,30 +57,46 @@ def trigger_pipeline(filename):
     return 
     
  
+# Flask web application to handle requests
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])  # Allow both GET and POST requests
 def main():
+    """
+    This function handles incoming requests from the Cloud Function, 
+    retrieves the relevant parameters, and triggers the pipeline to process the data file in GCS.
+    """
     data = request.get_json()
+
+    # Extract parameters from the Cloud Function message
+    global project_id, region, dataset_id
+    project_id = data.get('project_id')
+    region = data.get('region')
+    dataset_id = data.get('dataset_id')
     filename = data.get('file')
 
+    # If a filename is provided, trigger the pipeline
     if filename:
-        print(f'Filename received from the Cloud Function : {filename}')
+        print(f'Data received from the Cloud Function : file : {filename}, project_id : {project_id}, region : {region}, dataset_id : {dataset_id}')
         print("Triggering pipeline...")
         trigger_pipeline(filename)
         response = "Pipeline finished successfully!"
         print(response)
-        # Marquer pour arrêter le serveur après la réponse
+
+        # Shutdown the server after the response is sent
         request.environ['shutdown'] = True
         print("return message")
         return jsonify({'message': response}), 200
     else:
+        # If no filename is provided, return an error response
         return jsonify({'error': 'Missing filename parameter'}), 400
 
 
 @app.after_request
 def shutdown_after_request(response):
-    """Arrête le serveur après avoir traité la réponse."""
+    """
+    This function ensures the server is stopped after handling the request.
+    """
     if request.environ.get('shutdown'):
         os.kill(os.getpid(), signal.SIGINT)
     return response
--- dvtapp/manage_table.py
+++ dvtapp/manage_table.py
@@ -3,17 +3,19 @@ import datetime
 import time
 
 def delete_all_tables(client, project, dataset_id):
-    """Supprime toutes les tables d'un dataset BigQuery.
+    """
+    Deletes all tables in a specified BigQuery dataset.
 
     Args:
-        client: Un client BigQuery.
-        project: Le nom du projet.
-        dataset_id: L'ID du dataset.
+        client (bigquery.Client): A BigQuery client instance.
+        project (str): The Google Cloud project ID.
+        dataset_id (str): The ID of the dataset to clean.
     """
-
+    # Get the list of all table present in the dataset
     dataset_ref = client.dataset(dataset_id)
     tables = client.list_tables(dataset_ref)
 
+    # Remove all tables from the dataset
     for table in tables:
         table_id = table.table_id
         table_ref = dataset_ref.table(table_id)
@@ -22,29 +24,38 @@ def delete_all_tables(client, project, dataset_id):
         
 
 def create_table_name():
+    """
+    Generates a unique table name based on the current timestamp.
+
+    Returns:
+        str: A unique table name in the format `dvtapp_table_<timestamp>`.
+    """
     format="%d%m%Y%H%M%S"
-    maintenant = datetime.datetime.now()
-    table_name = str("dvtapp_table_"+maintenant.strftime(format))
+    now = datetime.datetime.now()
+    table_name = str("dvtapp_table_"+now.strftime(format))
     return table_name
 
+
 def create_table_if_not_exists(client, dataset_id, table_id, initial_schema=[]):
-    """Creates a BigQuery table if it doesn't exist.
+    """
+    Creates a BigQuery table if it does not already exist.
 
     Args:
-        client: A BigQuery client.
-        dataset_id: The ID of the dataset.
-        table_id: The ID of the table.
-        schema: The schema for the table.
+        client (bigquery.Client): A BigQuery client instance.
+        dataset_id (str): The ID of the dataset where the table should be created.
+        table_id (str): The desired ID of the table.
+        initial_schema (list, optional): A list of BigQuery schema fields. Defaults to an empty schema.
     """
-
+    # Identify the table and the dataset
     dataset_ref = client.dataset(dataset_id)
     table_ref = dataset_ref.table(table_id)
 
     try:
-        # Utilisez list_tables() pour vérifier l'existence de la table
+        # Check if a table already exists with the same name
         tables = client.list_tables(dataset=dataset_ref)
         table_exists = any(table.table_id == table_id for table in tables)
 
+        # Create the new table to load the data
         if not table_exists:
             table = bigquery.Table(table_ref, schema=initial_schema)
             table = client.create_table(table)
@@ -54,10 +65,23 @@ def create_table_if_not_exists(client, dataset_id, table_id, initial_schema=[]):
 
 
 def insert_data_with_retry(client, dataset_id, table_id, data):
+    """
+    Inserts a row into a BigQuery table, retry if necessary.
+
+    Args:
+        client (bigquery.Client): A BigQuery client instance.
+        dataset_id (str): The ID of the dataset containing the table.
+        table_id (str): The ID of the table to insert data into.
+        data (dict): The data row to insert.
+
+    Raises:
+        Exception: If the insertion fails after all retries.
+    """
     max_retries = 10
-    retry_delay = 1  # En secondes
+    retry_delay = 1  # Initial delay in seconds
 
     for attempt in range(max_retries):
+        # Try to insert row in the table
         try:
             table = client.get_table(f"{dataset_id}.{table_id}") 
             errors = client.insert_rows(table, [data])
@@ -66,6 +90,7 @@ def insert_data_with_retry(client, dataset_id, table_id, data):
             print(f"Row inserted: {data}")
             return
         except Exception as e:
+            # If a column doesn't exists, try again later, giving time to the new column to get created
             if "no such field" in str(e):
                 print(f"Waiting for column propagation. Retry in {retry_delay} seconds...")
                 time.sleep(retry_delay)
@@ -74,36 +99,52 @@ def insert_data_with_retry(client, dataset_id, table_id, data):
                 raise
 
 def add_column_to_table(client, dataset_id, table_id, column_name, SchemaField):
-    # print("add_column_to_table : ", column_name)
+    """
+    Adds a new column to an existing BigQuery table.
+
+    Args:
+        client (bigquery.Client): A BigQuery client instance.
+        dataset_id (str): The ID of the dataset containing the table.
+        table_id (str): The ID of the table to modify.
+        column_name (str): The name of the column to add.
+        SchemaField (bigquery.SchemaField): The schema field definition for the new column.
+    """
+
     table_ref = client.dataset(dataset_id).table(table_id)
     table = client.get_table(table_ref)
 
+    # Add a new column to the current table's schema
     hard_copy_schema = table.schema
-
     hard_copy_schema.append(SchemaField)
 
+    # Update the table's schema with the new one
     table.schema = hard_copy_schema
     table = client.update_table(table, ["schema"])
     table = client.get_table(table_ref) 
-    print("column added : ", column_name)
+    print("Column added : ", column_name)
 
 
 def compare_dict_to_schema(client, dataset_id, table_id, data_dict):
+    """
+    Compares a dictionary of data against a BigQuery table schema to find missing columns.
 
-    print("compare_dict_to_schema")
+    Args:
+        client (bigquery.Client): A BigQuery client instance.
+        dataset_id (str): The ID of the dataset containing the table.
+        table_id (str): The ID of the table to compare against.
+        data_dict (dict): The data dictionary to validate.
+
+    Returns:
+        list: A list of missing column names.
+    """
+    print("Comparing data dictionary to schema...")
     table_ref = client.dataset(dataset_id).table(table_id)
     table = client.get_table(table_ref)
     schema = table.schema
 
-    missing_columns = []
-    for key in data_dict.keys():
-        found = False
-        for field in schema:
-            if field.name == key:
-                found = True
-                break
-        if not found:
-            missing_columns.append(key)
-    print("missing columns : " + str(missing_columns))
+    # Check if a column is present in the JSON data and is missing in the table's schema
+    missing_columns = [key for key in data_dict.keys() if key not in {field.name for field in schema}]
+    
+    print("Missing columns : " + str(missing_columns))
     return missing_columns
 
